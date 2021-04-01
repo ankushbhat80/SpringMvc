66fd7a1c8e38a4f11d39e34075a42758b066c585
Ankush <ankushbhat80@gmail.com>
2021-04-01 15:51:19 +0530

    Test Commit for Spring mvc

--- src/main/java/com/journaldev/spring/Child.java
+++ src/main/java/com/journaldev/spring/Child.java
@@ -1,9 +1,7 @@
 package com.journaldev.spring;
 
 public class Child extends Parent {
-/*static final void m1() {
-	
-}*/
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
