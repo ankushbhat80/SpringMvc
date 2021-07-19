a1d27b71db26f2f68988696e84e6cac798999882
ankushbhat80 <ankushbhat80@gmail.com>
2021-07-19 19:23:14 +0530

    Update PersonDAOImpl.java

--- src/main/java/com/journaldev/spring/dao/PersonDAOImpl.java
+++ src/main/java/com/journaldev/spring/dao/PersonDAOImpl.java
@@ -15,6 +15,8 @@ import org.springframework.stereotype.Repository;
 
 import com.journaldev.spring.model.Person;
 
+//commit from master
+
 @Repository
 public class PersonDAOImpl implements PersonDAO {
 	
