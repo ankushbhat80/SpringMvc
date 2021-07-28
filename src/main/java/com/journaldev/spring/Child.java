dad68f9bbe667ffa7b505aae90502b3eb2d1b85c
ankushbhat <ankushbhat80@gmail.com>
2021-07-28 21:34:32 +0530

    commit commit in case of feature

--- src/main/java/com/journaldev/spring/Child.java
+++ src/main/java/com/journaldev/spring/Child.java
@@ -1,12 +1,11 @@
 package com.journaldev.spring;
 
 public class Child extends Parent {
-	
-	
-	
-
-
-
+	/**
+	 *
+	 * @param args
+	 * Changes made in feature
+	 */
 	//public static void main
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
